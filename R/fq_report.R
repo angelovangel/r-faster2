3b172d205604eee1784ca22b6fd64ce7df204da2
angelovangel <aangeloo@gmail.com>
2024-07-14 16:06:40 +0300

    html report deps

--- R/fq_report.R
+++ R/fq_report.R
@@ -3,19 +3,23 @@
 #' Generate html summary report for the fastq files in a folder
 #'
 #' @param inpath A path to folder containing fastq/fastq.gz files
-#' @return
+#' @param pattern Regex pattern to search for fastq files
+#' @param platform Sequencing platform used
+#' @param outfile name of html report file, default is rfaster2-report.html, written in the calling directory
 #' @export
 
-fq_report <- function(inpath) {
+fq_report <- function(inpath, pattern = 'fast(q|q.gz)$', platform = 'Nanopore', outfile = 'rfaster2-report.html') {
   calldir <- getwd()
   template <- paste0(system.file(package = 'rfaster2') , '/report.Rmd')
 
   rmarkdown::render(
     input = template,
-    output_file = 'rfaster2-report.html',
+    output_file = outfile,
     output_dir = calldir,
     params = list(
-      fastq_dir = inpath
+      fastq_dir = inpath,
+      fastq_pattern = pattern,
+      platform = platform
     )
   )
 }
--- R/rfaster2-package.R
+++ R/rfaster2-package.R
@@ -2,6 +2,15 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr %>%
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr summarise
+#' @importFrom kableExtra kbl
+#' @importFrom knitr kable
 #' @importFrom rmarkdown render
+#' @importFrom scales cut_si
+#' @importFrom scales label_number
+#' @importFrom sparkline sparkline
+#' @importFrom sparkline spk_chr
 ## usethis namespace: end
 NULL
